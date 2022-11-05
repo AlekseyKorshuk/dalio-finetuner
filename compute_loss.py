b76812a3450ced44900e3f201a294bf5af6ca7fc
Aleksey Korshuk <ale-kor02@mail.ru>
2022-11-06 00:10:12 +0300

    updates

--- compute_loss.py
+++ compute_loss.py
@@ -24,8 +24,12 @@ print(outputs.loss)
 labels = inputs["labels"]
 lm_logits = outputs.logits
 
+
+print(lm_logits.size())
 shift_logits = lm_logits[..., :-1, :].contiguous()
+
 shift_labels = labels[..., 1:].contiguous()  # Flatten the tokens
+print(shift_labels.size())
 loss_fct = CrossEntropyLoss(ignore_index=-1)
 loss = loss_fct(shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1))
 print(loss)
