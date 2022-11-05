754929752478309c3b5719d02543391cf687de20
Aleksey Korshuk <ale-kor02@mail.ru>
2022-11-05 22:59:12 +0300

    updates

--- test2.py
+++ test2.py
@@ -5,7 +5,7 @@ from torch import tensor
 model = AutoModelForCausalLM.from_pretrained("gpt2").to(0)
 tokenizer = AutoTokenizer.from_pretrained("gpt2")
 
-input_string = "User"
+input_string = "User:"
 
 inputs = tokenizer(input_string, return_tensors="pt").to(0)
 input_len = len(inputs["input_ids"][0])
