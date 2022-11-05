92e32a9b3667d024cd2e3053b4b431a60abd9b41
Aleksey Korshuk <ale-kor02@mail.ru>
2022-11-05 21:19:00 +0300

    updates

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from torch.nn import CrossEntropyLoss
 from transformers import AutoModelForCausalLM, AutoTokenizer
-import torch
+from torch import tensor
 
 model = AutoModelForCausalLM.from_pretrained("gpt2").to(0)
 tokenizer = AutoTokenizer.from_pretrained("gpt2")
@@ -8,19 +8,26 @@ tokenizer = AutoTokenizer.from_pretrained("gpt2")
 input_str = "User: Hello, how are you?\nBot:"
 output_str = " I am fine"
 
-inputs = tokenizer(input_str + output_str, return_tensors="pt", return_token_type_ids=True).to(0)
-inputs["labels"] = inputs["input_ids"]
+inputs = tokenizer(input_str, return_tensors="pt").to(0)
+input_len = len(inputs["input_ids"][0])
 
-# inputs = {
-#     "input_ids": torch.tensor([0, 0, 0, 0, 0], device="cuda:0"),
-#     "attention_mask": torch.tensor([1, 1, 1, 1, 1], device="cuda:0"),
-#     "labels": torch.tensor([0, 0, 0, 0, 0], device="cuda:0"),
-# }
+inputs = tokenizer(output_str, return_tensors="pt").to(0)
+output_len = len(inputs["input_ids"][0])
+
+inputs = {
+    'input_ids': tensor([[12982, 25, 18435, 11, 703, 389, 345, 30, 198, 20630, 25,
+                          314, 716, 3734]], device='cuda:0'),
+    'attention_mask': tensor([[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                               1, 1, 1]], device='cuda:0'),
+    'labels': tensor([[12982, 25, 18435, 11, 703, 389, 345, 30, 198, 20630, 25,
+                       314, 716, 3734]], device='cuda:0')
+}
 print(inputs)
 outputs = model(**inputs)
 print(outputs.loss)
 labels = inputs.labels
 lm_logits = outputs.logits
+print(lm_logits)
 shift_logits = lm_logits[..., :-1, :].contiguous()
 shift_labels = labels[..., 1:].contiguous()
 # Flatten the tokens
