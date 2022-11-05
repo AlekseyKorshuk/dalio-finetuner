30765aceb2f6d1d16be77b872111c9a6f4a77772
Aleksey Korshuk <ale-kor02@mail.ru>
2022-11-05 23:52:26 +0300

    updates

--- compute_loss.py
+++ compute_loss.py
@@ -9,8 +9,11 @@ tokenizer.pad_token = tokenizer.eos_token
 data = ["User", "User:"]
 inputs = tokenizer(data, return_tensors="pt", padding=True).to(0)
 inputs["labels"] = tensor(inputs["input_ids"].tolist().copy(), device="cuda:0")
-inputs["labels"][0][0] = 50256
 print(inputs)
+
+
+generated_output_ids = model.generate(**inputs, max_new_tokens=3, min_new_tokens=3, do_sample=False)
+print(generated_output_ids)
 
 outputs = model(**inputs)
 
