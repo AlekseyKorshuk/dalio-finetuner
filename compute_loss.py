d14fcd487ca1471fb1fd32a75c5a76bb4d2ce838
Aleksey Korshuk <ale-kor02@mail.ru>
2022-11-06 01:39:09 +0300

    updates

--- compute_loss.py
+++ compute_loss.py
@@ -16,38 +16,5 @@ output_lengths = [len(tokenizer(output_string).input_ids) for output_string in o
 for i in range(len(inputs["labels"])):
     for j in range(0, len(inputs["labels"][i]) - output_lengths[i]):
         inputs["labels"][i][j] = -100
-print(inputs)
 outputs = model(**inputs)
 print(outputs.loss)
-
-labels = inputs["labels"]
-lm_logits = outputs.logits
-
-shift_logits = lm_logits[..., :-1, :].contiguous()
-shift_logits = shift_logits.tolist()
-for i in range(len(shift_logits)):
-    shift_logits[i] = shift_logits[i][-output_lengths[i]:]
-shift_logits = cat([tensor(data, device="cuda:0") for data in shift_logits])
-
-shift_labels = labels[..., 1:].contiguous()
-shift_labels = shift_labels.tolist()
-for i in range(len(shift_labels)):
-    shift_labels[i] = shift_labels[i][-output_lengths[i]:]
-shift_labels = cat([tensor(data, device="cuda:0") for data in shift_labels])
-
-loss_fct = CrossEntropyLoss(ignore_index=-1)
-loss = loss_fct(shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1))
-print(loss)
-
-labels = inputs["labels"]
-lm_logits = outputs.logits
-shift_logits = lm_logits[..., :-1, :].contiguous()
-
-shift_labels = labels[..., 1:].contiguous()
-for i in range(len(shift_labels)):
-    for j in range(0, len(shift_labels[i]) - output_lengths[i]):
-        shift_labels[i][j] = -100
-
-loss_fct = CrossEntropyLoss(ignore_index=-100)
-loss = loss_fct(shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1))
-print(loss)
