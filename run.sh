490a2f61c09af871331f6ff5723552da2c8743c6
Aleksey Korshuk <ale-kor02@mail.ru>
2022-11-05 20:31:38 +0300

    updates

--- run.sh
+++ run.sh
@@ -4,8 +4,6 @@ python3 run_clm_no_trainer.py \
     --dataset_config_name wikitext-2-raw-v1 \
     --per_device_train_batch_size 8 \
     --per_device_eval_batch_size 8 \
-    --do_train \
-    --do_eval \
     --output_dir /tmp/test-clm \
     --num_train_epochs 1 \
     -with_tracking
