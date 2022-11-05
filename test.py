df7f7f53c78aab3591da5fc2d52c0d4622a123d1
Aleksey Korshuk <ale-kor02@mail.ru>
2022-11-05 21:11:33 +0300

    updates

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ tokenizer = AutoTokenizer.from_pretrained("gpt2")
 input_str = "User: Hello, how are you?\nBot:"
 output_str = ""
 
-inputs = tokenizer(input_str, return_tensors="pt").to(0)
+inputs = tokenizer(input_str, return_tensors="pt", return_token_type_ids=True).to(0)
 
 # inputs = {
 #     "input_ids": torch.tensor([0, 0, 0, 0, 0], device="cuda:0"),
@@ -17,6 +17,13 @@ inputs = tokenizer(input_str, return_tensors="pt").to(0)
 print(inputs)
 outputs = model(**inputs)
 print(outputs.loss)
+labels = inputs.labels
+lm_logits = outputs.logits
+shift_logits = lm_logits[..., :-1, :].contiguous()
+shift_labels = labels[..., 1:].contiguous()
+# Flatten the tokens
+loss_fct = CrossEntropyLoss()
+loss = loss_fct(shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1))
 
 #
 # inputs = {
