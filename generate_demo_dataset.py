df27d9172d6c07dfbcbf82db1b0245ddf190b598
Aleksey Korshuk <ale-kor02@mail.ru>
2022-11-06 13:54:22 +0300

    updates

--- generate_demo_dataset.py
+++ generate_demo_dataset.py
@@ -3,34 +3,34 @@ from datasets import load_dataset, Dataset, DatasetDict
 dataset = load_dataset("amazon_reviews_multi", "en")
 
 train_data = {
-    "input_string": [],
-    "output_string": []
+    "input_text": [],
+    "output_text": []
 }
 for i in range(1000):
-    input_string = f'Title: {dataset["train"][i]["review_title"]}\nBody:'
-    train_data["input_string"].append(input_string)
-    output_string = f" {dataset['train'][i]['review_body']}\n"
-    train_data["output_string"].append(output_string)
+    input_text = f'Title: {dataset["train"][i]["review_title"]}\nBody:'
+    train_data["input_text"].append(input_text)
+    output_text = f" {dataset['train'][i]['review_body']}\n"
+    train_data["output_text"].append(output_text)
 
 validation_data = {
-    "input_string": [],
-    "output_string": []
+    "input_text": [],
+    "output_text": []
 }
 for i in range(100):
-    input_string = f'Title: {dataset["validation"][i]["review_title"]}\nBody:'
-    validation_data["input_string"].append(input_string)
-    output_string = f" {dataset['validation'][i]['review_body']}\n"
-    validation_data["output_string"].append(output_string)
+    input_text = f'Title: {dataset["validation"][i]["review_title"]}\nBody:'
+    validation_data["input_text"].append(input_text)
+    output_text = f" {dataset['validation'][i]['review_body']}\n"
+    validation_data["output_text"].append(output_text)
 
 test_data = {
-    "input_string": [],
-    "output_string": []
+    "input_text": [],
+    "output_text": []
 }
 for i in range(10):
-    input_string = f'Title: {dataset["test"][i]["review_title"]}\nBody:'
-    test_data["input_string"].append(input_string)
-    output_string = f" {dataset['test'][i]['review_body']}\n"
-    test_data["output_string"].append(output_string)
+    input_text = f'Title: {dataset["test"][i]["review_title"]}\nBody:'
+    test_data["input_text"].append(input_text)
+    output_text = f" {dataset['test'][i]['review_body']}\n"
+    test_data["output_text"].append(output_text)
 
 
 ds = DatasetDict(
