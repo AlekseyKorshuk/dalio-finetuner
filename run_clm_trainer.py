9cb47e770ee5c51e9e64754975d3e9dd9e7cfc38
Aleksey Korshuk <ale-kor02@mail.ru>
2022-11-06 17:58:13 +0300

    updates

--- run_clm_trainer.py
+++ run_clm_trainer.py
@@ -466,7 +466,24 @@ def main():
         if training_args.do_eval and not is_torch_tpu_available()
         else None,
     )
-    torch.set_autocast_cache_enabled(False)
+    torch.set_autocast_cache_enabled(True)
+
+    if training_args.do_eval:
+        logger.info("*** Evaluate ***")
+
+        metrics = trainer.evaluate()
+
+        max_eval_samples = data_args.max_eval_samples if data_args.max_eval_samples is not None else len(eval_dataset)
+        metrics["eval_samples"] = min(max_eval_samples, len(eval_dataset))
+        try:
+            perplexity = math.exp(metrics["eval_loss"])
+        except OverflowError:
+            perplexity = float("inf")
+        metrics["perplexity"] = perplexity
+
+        print("METRICS:", metrics)
+        trainer.log_metrics("eval", metrics)
+        trainer.save_metrics("eval", metrics)
 
     # Training
     if training_args.do_train:
