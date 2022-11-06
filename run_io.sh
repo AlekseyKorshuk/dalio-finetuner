2fe27644201d6c3de7cffbb06c53c0b4a7920aba
Aleksey Korshuk <ale-kor02@mail.ru>
2022-11-06 15:32:30 +0300

    updates

--- run_io.sh
+++ run_io.sh
@@ -1,9 +1,9 @@
 python3 run_clm_io.py \
     --model_name_or_path facebook/opt-6.7b \
     --dataset_name AlekseyKorshuk/amazon-reviews-input-output \
-    --per_device_train_batch_size 8 \
+    --per_device_train_batch_size 1 \
+    --gradient_accumulation_steps 8 \
     --per_device_eval_batch_size 8 \
     --output_dir /tmp/test-clm \
     --num_train_epochs 5 \
-    --with_tracking \
-    --overwrite_cache
+    --with_tracking
