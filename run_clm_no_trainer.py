0f408f159fbf0bd8d8495ed809a59750a7602159
Aleksey Korshuk <ale-kor02@mail.ru>
2022-11-06 03:13:03 +0300

    updates

--- run_clm_no_trainer.py
+++ run_clm_no_trainer.py
@@ -340,7 +340,7 @@ def main():
             raw_datasets["train"] = load_dataset(
                 args.dataset_name,
                 args.dataset_config_name,
-                split=f"train[{85}%:]",
+                split=f"train[{args.validation_split_percentage}%:]",
             )
     else:
         data_files = {}
