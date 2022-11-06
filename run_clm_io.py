f6073798cbd7e13f83423c1eb1214280f6d62c7d
Aleksey Korshuk <ale-kor02@mail.ru>
2022-11-06 15:03:52 +0300

    updates

--- run_clm_io.py
+++ run_clm_io.py
@@ -252,7 +252,7 @@ def generate_table(model, tokenizer, test_dataset):
     }
     for sample in input_texts:
         inputs = tokenizer(sample, return_tensors="pt").to(0)
-        output_ids = model.generate(**inputs, max_new_tokens=64, eos_token_id=198)
+        output_ids = model.generate(**inputs, max_new_tokens=64, eos_token_id=50118)
         output = tokenizer.decode(output_ids[0][len(inputs.input_ids[0]):])
         table["output"].append(output)
     df = DataFrame(table)
