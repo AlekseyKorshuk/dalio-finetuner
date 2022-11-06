9012919b3dddf853645e4ab4c9a330182fb0be7e
Aleksey Korshuk <ale-kor02@mail.ru>
2022-11-06 15:52:15 +0300

    updates

--- run_clm_io.py
+++ run_clm_io.py
@@ -277,7 +277,7 @@ def main():
         accelerator_log_kwargs["log_with"] = args.report_to
         accelerator_log_kwargs["logging_dir"] = args.output_dir
 
-    accelerator = Accelerator(gradient_accumulation_steps=args.gradient_accumulation_steps, **accelerator_log_kwargs)
+    accelerator = Accelerator(fp16=True, gradient_accumulation_steps=args.gradient_accumulation_steps, **accelerator_log_kwargs)
 
     # Make one log on every process with the configuration for debugging.
     logging.basicConfig(
