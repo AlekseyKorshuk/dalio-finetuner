fad19ac7ad016666fb5f51e3476e9962963ceac9
Aleksey Korshuk <ale-kor02@mail.ru>
2022-11-06 03:14:22 +0300

    updates

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 python3 run_clm_no_trainer.py \
     --model_name_or_path facebook/opt-350m \
-    --dataset_name ChaiML/food_reviews \
+    --dataset_name wikitext \
+    --dataset_config_name wikitext-2-raw-v1 \
     --per_device_train_batch_size 8 \
     --per_device_eval_batch_size 8 \
     --output_dir /tmp/test-clm \
