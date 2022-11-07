23833d4d51adf9675af866b320d336ac833fb627
Aleksey Korshuk <ale-kor02@mail.ru>
2022-11-07 23:57:25 +0300

    updates

--- colossalai_train/run_clm.sh
+++ colossalai_train/run_clm.sh
@@ -17,7 +17,6 @@ torchrun \
   --output_dir $PWD \
   --mem_cap ${MEMCAP} \
   --per_device_train_batch_size ${BS} 2>&1 | tee ./logs/colo_${MODEL}_bs_${BS}_cap_${MEMCAP}_gpu_${GPUNUM}.log \
-  --overwrite_output_dir \
   --do_train \
   --do_eval \
   --logging_strategy steps \
