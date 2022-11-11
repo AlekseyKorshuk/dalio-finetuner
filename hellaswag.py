0eb0802454c4dedcfb83ddb52e58aefae0c592ba
Aleksey Korshuk <ale-kor02@mail.ru>
2022-11-11 22:59:13 +0300

    updates

--- hellaswag.py
+++ hellaswag.py
@@ -2,6 +2,7 @@ import logging
 import math
 
 from datasets import load_dataset
+from tqdm import tqdm
 from transformers import TrainerCallback
 import numpy as np
 import torch
@@ -20,7 +21,6 @@ class HellaswagCallback(TrainerCallback):
         self.ds = load_hellaswag_dataset()
 
     def on_evaluate(self, args, state, control, **kwargs):
-        print("Model device:", kwargs.get('model').device)
         if state.global_step != state.max_steps:
             return
 
@@ -46,56 +46,20 @@ def load_hellaswag_dataset():
 
 def score_hellaswag(dataset, model, tokenizer, num_prompts, params):
     inputs = dataset['ctx_a'][:num_prompts]
-    endings = dataset['endings'][:num_prompts]
-    labels = dataset['label'][:num_prompts]
 
     likelihoods = []
-    for ins, ends in zip(inputs, endings):
-        scores = [log_likelihood(model, tokenizer, ins, e, params) for e in ends]
-        likelihoods.append(scores)
+    is_match = []
+    for input_text in tqdm(inputs, desc='HellaSwag'):
+        inputs = tokenizer(input_text, return_tensors="pt").to(model.device)
+        input_ids = inputs.input_ids[0][1:]
+        output = model(**inputs)
+        output_probs = output.logits.softmax(-1)[0]
+        for probs, input_id in zip(output_probs, input_ids):
+            prob = probs[input_id]
+            likelihoods.append(math.log(prob) if prob > 0 else -np.inf)
+            is_match.append(probs.argmax() == input_id)
 
-    accuracy = calculate_accuracy(labels, likelihoods)
-    perplexity = calculate_perplexity(labels, likelihoods)
+    accuracy = sum(is_match) / len(is_match)
+    perplexity = np.exp(-np.mean(likelihoods))
 
     return accuracy, perplexity
-
-
-def calculate_perplexity(labels, likelihoods):
-    scores = []
-    for label, likelihood in zip(labels, likelihoods):
-        completion = likelihood[int(label)]
-        scores.append(np.exp(-np.mean(completion)))
-    return np.mean(scores)
-
-
-def calculate_accuracy(correct_labels, log_likelihoods):
-    correct_count = count_correct_labels(correct_labels, log_likelihoods)
-    return correct_count / len(correct_labels)
-
-
-def count_correct_labels(correct_labels, log_likelihoods):
-    correct_count = 0
-    for label, likelihoods in zip(correct_labels, log_likelihoods):
-        correct_count += int(label) == np.argmax(likelihoods)
-    return correct_count
-
-
-def log_likelihood(model, tokenizer, text, output_text, params):
-    input_tokens = tokenizer(text, return_tensors="pt").to(model.device)['input_ids']
-    output_tokens = tokenizer(output_text, return_tensors="pt").to(model.device)['input_ids']
-
-    args = params.copy()
-    args = {
-        'return_dict_in_generate': True,
-        'output_scores': True,
-        'max_new_tokens': 1
-    }
-
-    logprobs = []
-    for token in output_tokens[0]:
-        output = model.generate(input_tokens, **args)
-        output_probs = torch.stack(output.scores, dim=1).softmax(-1)[0][0]
-        prob = output_probs[token]
-        logprobs.append(math.log(prob) if prob > 0 else -np.inf)
-        input_tokens = torch.cat((input_tokens, token.resize(1, 1)), dim=1)
-    return logprobs
