14878bf4bf93e580992b3d63c950de061e82665d
Aleksey Korshuk <ale-kor02@mail.ru>
2022-11-14 13:19:22 +0300

    updates

--- run_clm_trainer.py
+++ run_clm_trainer.py
@@ -11,6 +11,8 @@ import datasets
 import torch
 import tqdm
 from datasets import load_dataset
+from transformers.modeling_utils import unwrap_model
+from transformers.trainer import TRAINING_ARGS_NAME
 from wandb import Table
 
 import evaluate
@@ -27,11 +29,11 @@ from transformers import (
     TrainingArguments,
     default_data_collator,
     is_torch_tpu_available,
-    set_seed,
+    set_seed, PreTrainedModel,
 )
 from transformers.testing_utils import CaptureLogger
 from transformers.trainer_utils import get_last_checkpoint
-from transformers.utils import check_min_version, send_example_telemetry
+from transformers.utils import check_min_version, send_example_telemetry, WEIGHTS_NAME
 from transformers.utils.versions import require_version
 import hellaswag
 
@@ -190,6 +192,37 @@ class CustomArguments:
     casual_loss: bool = field(
         default=False, metadata={"help": "Whenever to calculate the loss on the full text."}
     )
+
+
+class CustomTrainer(Trainer):
+    def _save(self, output_dir: Optional[str] = None, state_dict=None):
+        # If we are executing this function, we are the process zero, so we don't check for that.
+        output_dir = output_dir if output_dir is not None else self.args.output_dir
+        os.makedirs(output_dir, exist_ok=True)
+        logger.info(f"Saving model checkpoint to {output_dir}")
+        # Save a trained model and configuration using `save_pretrained()`.
+        # They can then be reloaded using `from_pretrained()`
+        if not isinstance(self.model, PreTrainedModel):
+            print("Saving model outside of PreTrainedModel")
+            if isinstance(unwrap_model(self.model), PreTrainedModel):
+                if state_dict is None:
+                    state_dict = self.model.state_dict()
+                print("Unwrapping model")
+                unwrap_model(self.model).save_pretrained(output_dir, state_dict=state_dict)
+            else:
+                logger.info("Trainer.model is not a `PreTrainedModel`, only saving its state dict.")
+                if state_dict is None:
+                    state_dict = self.model.state_dict()
+                torch.save(state_dict, os.path.join(output_dir, WEIGHTS_NAME))
+        else:
+            print("Saving model inside of PreTrainedModel")
+            self.model.save_pretrained(output_dir, state_dict=state_dict)
+        if self.tokenizer is not None:
+            self.tokenizer.save_pretrained(output_dir)
+
+        print("State dict:", state_dict)
+        # Good practice: save your training arguments together with the trained model
+        torch.save(self.args, os.path.join(output_dir, TRAINING_ARGS_NAME))
 
 
 def main():
@@ -511,7 +544,7 @@ def main():
     # callbacks.append(callback)
 
     # Initialize our Trainer
-    trainer = Trainer(
+    trainer = CustomTrainer(
         model=model,
         args=training_args,
         train_dataset=train_dataset if training_args.do_train else None,
@@ -572,6 +605,7 @@ def main():
     else:
         trainer.create_model_card(**kwargs)
 
+    import pdb; pdb.set_trace()
 
 def _mp_fn(index):
     # For xla_spawn (TPUs)
